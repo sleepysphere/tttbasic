22ccae4261fce84f7d9f199a3c3ce356c6f9bdb1
DuyVo <duyvo280405@gmail.com>
2026-06-01 09:30:53 +0700

    first commit

---
+++ src/main/java/com/duyvo/pe26/tttbasic/App.java
@@ -0,0 +1,10 @@
+package com.duyvo.pe26.tttbasic;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
